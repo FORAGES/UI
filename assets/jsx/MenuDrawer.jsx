7d929cf6a46950dc382329c68a3fbc015ad10c40
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-09-28 19:13:48 -0700

    Favor e.preventDefault over href="javascript:void(0)" trick.

--- assets/jsx/MenuDrawer.jsx
+++ assets/jsx/MenuDrawer.jsx
@@ -4,30 +4,33 @@ var MenuDrawer = React.createClass({
   
   onClickInvert: function(e) {
     this.context.main.setState({ invert: !this.context.main.state.invert })
+    e.preventDefault()
   },
   
   onClickLeftward: function(e) {
     this.context.main.setState({ invert: !this.context.main.state.invert })
+    e.preventDefault()
   },
   
   onClickRightward: function(e) {
     this.context.main.setState({ invert: !this.context.main.state.invert })
+    e.preventDefault()
   },
   
   render: function() {
     return (<div style={{ width: "100vw" }}>
       <div style={{ position: "absolute", top: 0, left: 20 }}>
-        <h2><a href="javascript:void(0)" onClick={this.onClickInvert}>{"☯"}</a></h2>
+        <h2><a href="" onClick={this.onClickInvert}>{"☯"}</a></h2>
       </div>
       <h2>
-        <a href="javascript:void(0)" onClick={this.onClickLeftward}>{"⚝"}</a>
+        <a href="" onClick={this.onClickLeftward}>{"⚝"}</a>
         <span className="ddim">{" ⇜ "}</span>
         <span className="dim">E X P</span>{" "} L O R E
         <span className="ddim">{" ⇝ "}</span>
-        <a href="javascript:void(0)" onClick={this.onClickRightward}>{"⚝"}</a>
+        <a href="" onClick={this.onClickRightward}>{"⚝"}</a>
       </h2>
       <div style={{ position: "absolute", top: 0, right: 20 }}>
-        <h2><a href="javascript:void(0)" onClick={this.onClickInvert}>{"Ω"}</a></h2>
+        <h2><a href="" onClick={this.onClickInvert}>{"Ω"}</a></h2>
       </div>
     </div>)
   }
